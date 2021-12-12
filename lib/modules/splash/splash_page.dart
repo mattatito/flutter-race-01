0e8b32008fd48b8ce1d34d92c93493c6bb252869
matheus.geiser <matheusgeiser@gmail.com>
2021-12-11 23:38:09 -0300

    finishing splash page layout

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,11 +1,16 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_race/shared/theme/app_theme.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppTheme.colors.background,
+      body: Center(
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
   }
 }
